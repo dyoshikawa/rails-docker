806f61b1fe3fe6aae4a9384e68bd9e52d454f2c9
dyoshikawa <dyoshikawa1993@gmail.com>
2018-02-21 00:27:12 +0900

    second commit

--- docker/rails/docker-entrypoint.sh
+++ docker/rails/docker-entrypoint.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+if [ -f Gemfile ]; then
+    rm -rf /rails-pre
+    bundle install
+    bundle update
+else
+    shopt -s dotglob
+    mv /rails-pre/* /rails-app/
+    rmdir /rails-pre
+fi
+chown -R 1000:1000 /rails-app
+rails s -b 0.0.0.0
