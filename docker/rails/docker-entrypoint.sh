9dd6f1d960184717daec9f3f7ebfba6021f2afa9
dyoshikawa <dyoshikawa1993@gmail.com>
2018-02-21 00:43:00 +0900

    fix docker-compose.yml context書き間違い修正

--- docker/rails/docker-entrypoint.sh
+++ docker/rails/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 if [ -f Gemfile ]; then
-    rm -rf /rails-pre
+    sudo rm -rf /rails-pre
     bundle install
     bundle update
 else
-    shopt -s dotglob
-    mv /rails-pre/* /rails-app/
-    rmdir /rails-pre
+    sudo shopt -s dotglob
+    sudo mv /rails-pre/* /rails-app/
+    sudo rmdir /rails-pre
 fi
-chown -R 1000:1000 /rails-app
+sudo chown -R 1000:1000 /rails-app
 rails s -b 0.0.0.0
