792be922370589e849f2e22b604479a2322dc78f
dyoshikawa <dyoshikawa1993@gmail.com>
2018-02-21 01:13:33 +0900

    fix とりあえず動いた

--- docker/rails/docker-entrypoint.sh
+++ docker/rails/docker-entrypoint.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-if [ -f Gemfile ]; then
-    sudo rm -rf /rails-pre
-    bundle install
-    bundle update
-else
-    sudo shopt -s dotglob
-    sudo mv /rails-pre/* /rails-app/
-    sudo rmdir /rails-pre
-fi
-sudo chown -R 1000:1000 /rails-app
+#if [ -f Gemfile ]; then
+#    rm -rf /rails-pre
+#    bundle install
+#    bundle update
+#else
+#    mv /rails-pre/* /rails-app/
+#    mv /rails-pre/.?** /rails-app/
+#    rmdir /rails-pre
+#fi
 rails s -b 0.0.0.0
